3f1ea8d47be6ab7550d5481b61bd405019b273d5
DingTianXiu <295434665@qq.com>
2015-11-09 18:39:45 +0800

    完成登录页面

---
+++ web/login/login.js
@@ -0,0 +1,14 @@
+/**
+ * Created by mpb on 15/11/5.
+ */
+
+var UserInformation = AV.Object.extend('userInfomation'),
+    Article = AV.Object.extend('Article'),
+    RecommendArticle = AV.Object.extend('RecommendArticle'),
+    RecommendCollection = AV.Object.extend('RecommendCollection'),
+    Collection = AV.Object.extend('Collection');
+
+angular.module('muApp.Login',[])
+    .controller('loginCtrl',function($scope){
+
+    })
